53ad7c1b8f1e951be42dfda52b2cd8a97369b5b1
Lakshmi K P <lakshmikp@yahoo.com>
2015-07-11 22:18:06 -0700

    Enhanced to automate download and unzipping

--- data.R
+++ data.R
@@ -1,4 +1,8 @@
-fp <- file(".//data//household_power_consumption.txt")
+
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+fp <- unzip(temp)
 rawData <- read.table(fp, header = TRUE, sep = ";",  na.strings = "?")
 rawData$Date <- as.Date(rawData$Date, format = "%d/%m/%Y")
 analysisData <- subset(rawData, (Date >= "2007-02-01" & Date <= "2007-02-02"))
